87370187c732317d0f3282caba020c002df19bfb
WillisLi <42555076+WillisLi@users.noreply.github.com>
2018-10-29 20:59:02 -0700

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -2,7 +2,7 @@ class Chick implements Animal {
      private String myType;     
      private String mySound;
      private String mySound2;
-     public Chick(String type, String sound, String sound2)     {         
+     public Chick(String type, String sound, String sound2) {         
          myType = type;         
          mySound = sound;
          mySound2 = sound2;
